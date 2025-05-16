4dc92dabfd5084388e4d57fcd1601f8c1b615e5d
ju0204 <p1106000@gmail.com>
2025-05-16 17:35:59 +0900

    test: Git 연동 확인용 커밋

--- src/main/java/likelion/team6th/fortune/repository/temp.java
+++ src/main/java/likelion/team6th/fortune/repository/temp.java
@@ -1,4 +1,9 @@
 package likelion.team6th.fortune.repository;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+
 public class temp {
+	public static void printTest() {
+        System.out.println("연동테스트");
+    }
 }
